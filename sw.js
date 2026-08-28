d90e67a8755ed145103f3b277efc50ae1b5c4c0c
bluebluegogo5134-spec <bluebluegogo5134@gmail.com>
2026-08-28 13:23:05 +0800

    Refresh PWA cache and load latest single-screen build

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='creative-planet-v2';const A=['./','./index.html','./manifest.webmanifest','./hero1.jpg','./hero2.jpg','./hero3.jpg','./hero4.jpg','./hero5.jpg','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='creative-planet-v4';
+const ASSETS=['./','./index.html','./styles.css?v=3','./app.js?v=3','./manifest.webmanifest','./hero1.jpg','./hero2.jpg','./hero3.jpg','./hero4.jpg','./hero5.jpg','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'){e.respondWith(fetch(r).catch(()=>caches.match('./index.html')));return}e.respondWith(fetch(r).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(r,copy));return resp}).catch(()=>caches.match(r)))});
