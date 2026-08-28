fee869ae9fa3a379d71dd7b9822fcd6156b698ed
bluebluegogo5134-spec <bluebluegogo5134@gmail.com>
2026-08-28 12:58:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='creative-planet-v2';const A=['./','./index.html','./manifest.webmanifest','./hero1.jpg','./hero2.jpg','./hero3.jpg','./hero4.jpg','./hero5.jpg','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
